7dbecbdd8229e71bef5ea116655d1c56ecf7c6dc
Riku Laajala <riku.laajala@gmail.com>
2021-08-29 19:40:40 +0300

    Fixes

--- lib/newsbloat_web/live/feed_live/feed_list_component.ex
+++ lib/newsbloat_web/live/feed_live/feed_list_component.ex
@@ -22,7 +22,7 @@ defmodule NewsbloatWeb.FeedLive.FeedListComponent do
   end
 
   def render(assigns) do
-    get_feed_title = fn (feed, unread_count) ->
+    get_feed_title = fn feed, unread_count ->
       if unread_count != 0 do
         feed.title <> " (" <> to_string(unread_count) <> ")"
       else
@@ -31,21 +31,16 @@ defmodule NewsbloatWeb.FeedLive.FeedListComponent do
     end
 
     ~L"""
-    <section class="fixed top-0 p-4">
+    <section class="fixed top-0 p-4 w-64 h-screen">
     <h3>Feeds</h3>
 
     <ul>
       <%= for %{ :feed => feed, :unread_count => unread_count } <- @feeds do %>
         <li>
-        <%= live_redirect get_feed_title.(feed, unread_count),
-          to: Routes.feed_show_path(@socket, :show, feed),
-          class: if @current_feed.id == feed.id, do: "w-full active", else: "w-full"
-        %>
-        </span>
+          <%= live_redirect get_feed_title.(feed, unread_count), to: Routes.feed_show_path(@socket, :show, feed), class: if @current_feed.id == feed.id, do: "w-full truncate active", else: "truncate w-full" %>
         </li>
 
-
-      <%= end %>
+      <% end %>
       <li>
         <span><%= live_patch "New Feed", to: Routes.feed_index_path(@socket, :new) %></span>
       </li>

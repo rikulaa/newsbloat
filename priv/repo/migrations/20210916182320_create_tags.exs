0dd2ea01ff56c9faa501c7b4c4fa87fc46b5b3bd
Riku Laajala <riku.laajala@gmail.com>
2021-09-17 21:36:24 +0300

    Add 'Tag' struct
    Added 'Many To Many' relationships between Tag <-> Feed|FeedItem.

---
+++ priv/repo/migrations/20210916182320_create_tags.exs
@@ -0,0 +1,29 @@
+defmodule Newsbloat.Repo.Migrations.CreateTags do
+  use Ecto.Migration
+
+  def change do
+    create table(:tags) do
+      add :title, :string
+
+      timestamps()
+    end
+
+    create unique_index(:tags, [:title])
+
+    # Primary key and timestamps are not required if
+    # using many_to_many without schemas
+    create table("feed_tags", primary_key: false) do
+      add :feed_id, references(:feeds)
+      add :tag_id, references(:tags)
+    # timestamps()
+    end
+
+    # Primary key and timestamps are not required if
+    # using many_to_many without schemas
+    create table("item_tags", primary_key: false) do
+      add :item_id, references(:items)
+      add :tag_id, references(:tags)
+      # timestamps()
+    end
+  end
+end

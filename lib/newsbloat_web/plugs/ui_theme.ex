70051984b817b2bfc930ab97423037ad27867417
Riku Laajala <riku.laajala@gmail.com>
2021-10-05 22:02:32 +0300

    feat: store chosen theme in cookie
    So that value persists through full page refreshes

--- lib/newsbloat_web/plugs/ui_theme.ex
+++ lib/newsbloat_web/plugs/ui_theme.ex
@@ -4,12 +4,17 @@ defmodule NewsbloatWeb.Plugs.UITheme do
   def init(default), do: default
 
   def call(%Plug.Conn{query_params: %{"ui_theme" => ui_theme}} = conn, _default) do
-    assign(conn, :ui_theme, ui_theme)
+    conn
     |> put_session(:ui_theme, ui_theme)
+    |> put_resp_cookie("ui-theme", ui_theme)
   end
 
-  def call(conn, default) do
-    assign(conn, :ui_theme, "dark")
-    |> put_session(:ui_theme, "dark")
+  def call(conn, _default) do
+    # TODO: Should initially respect the browsers 'prefers color scheme' value
+    default_ui_theme = conn.cookies |> Map.get("ui-theme", "dark")
+
+    conn
+    |> put_session(:ui_theme, default_ui_theme)
+    |> put_resp_cookie("ui-theme", default_ui_theme)
   end
 end

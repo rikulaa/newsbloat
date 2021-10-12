159f508970b150460461102cbf3a44b7d92bfe56
Riku Laajala <riku.laajala@gmail.com>
2021-10-12 21:06:07 +0300

    feat: command and jobs for resetting the cache
    Cache will be cleared every 24 hours

---
+++ lib/mix/tasks/rss/reset_cache.ex
@@ -0,0 +1,14 @@
+defmodule Mix.Tasks.Rss.ResetCache do
+  use Mix.Task
+
+  alias Newsbloat.Cache
+
+  @requirements ["app.start"]
+  @shortdoc "Reset application cache"
+
+  @impl Mix.Task
+  def run(_args) do
+    IO.puts "Clearing cache..."
+    Cache.reset()
+  end
+end

0eee4a3a7f4668f3d97ff236a58df463938a225b
Riku Laajala <riku.laajala@gmail.com>
2023-11-08 22:13:35 +0200

    Run 'cronjobs' only in prod

--- lib/newsbloat/application.ex
+++ lib/newsbloat/application.ex
@@ -6,23 +6,27 @@ defmodule Newsbloat.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
-      # Start the Ecto repository
-      Newsbloat.Repo,
-      # Start the Telemetry supervisor
-      NewsbloatWeb.Telemetry,
-      # Start the PubSub system
-      {Phoenix.PubSub, name: Newsbloat.PubSub},
-      # Start the Endpoint (http/https)
-      NewsbloatWeb.Endpoint,
-      # Start a worker by calling: Newsbloat.Worker.start_link(arg)
-      # {Newsbloat.Worker, arg}
+    prod_jobs = [
       {Newsbloat.Jobs.FetchNewFeedItems, name: NewsbloatWeb.Jobs.FetchNewFeedItems},
       {Newsbloat.Jobs.ResetCache, name: Newsbloat.Jobs.ResetCache},
-      {Newsbloat.Jobs.CleanOrphanedTags, name: Newsbloat.Jobs.CleanOrphanedTags},
-      # Cache
-      {Newsbloat.Cache, name: Newsbloat.Cache}
+      {Newsbloat.Jobs.CleanOrphanedTags, name: Newsbloat.Jobs.CleanOrphanedTags}
     ]
+
+    children =
+      [
+        # Start the Ecto repository
+        Newsbloat.Repo,
+        # Start the Telemetry supervisor
+        NewsbloatWeb.Telemetry,
+        # Start the PubSub system
+        {Phoenix.PubSub, name: Newsbloat.PubSub},
+        # Start the Endpoint (http/https)
+        NewsbloatWeb.Endpoint,
+        # Start a worker by calling: Newsbloat.Worker.start_link(arg)
+        # {Newsbloat.Worker, arg}
+        # Cache
+        {Newsbloat.Cache, name: Newsbloat.Cache}
+      ] ++ if Mix.env() == :prod, do: prod_jobs, else: []
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
